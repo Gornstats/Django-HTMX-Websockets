df31c1175fbca9f59e421040719a0760ca6a937d
Jeremy Gorniak <railspider@gmail.com>
2023-11-26 22:24:05 +1100

    Update consumers.py

--- core/consumers.py
+++ core/consumers.py
@@ -1,8 +1,19 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
+        # print(self.channel_name) - gives unique name of client consumer
+        self.GROUP_NAME = 'user-notifications' #fixed group for testing that all users are added to
+        
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.GROUP_NAME, self.channel_name
+        )       
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.GROUP_NAME, self.channel_name
+        )
